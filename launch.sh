09698f36842f61bdf8b2e72935eae1c9b14313d6
Skullfurious <34455502+Skullfurious@users.noreply.github.com>
2022-02-04 19:57:07 -0400

    Cleanup

--- launch.sh
+++ launch.sh
@@ -1,26 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x
 
-map=$1
-seshname=$2
-port=$3
-query=$4
-rcon=$5
-adminpass=$6
-pass=$7
-playercount=$8
-difficulty=$9
+map=${1}
+seshname=${2}
+port=${3}
+query=${4}
+rcon=${5}
+adminpass=${6}
+pass=${7}
+playercount=${8}
+difficulty=${9}
 officialdifficulty=${10}
-
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
-echo $6
-echo $7
-echo $8
-echo $9
-echo ${10}
 
 steamcmd +force_install_dir ./ark +login anonymous +app_update 376030 +quit
 
